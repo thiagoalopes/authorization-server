28e6f15496b55756332c60ef677274ad5ec148c9
Thiago A. Lopes <thiagolopesbv@gmail.com>
2023-11-29 06:37:52 -0400

    Início

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('login', 'App\Http\Controllers\AuthController@login')
+->name('login');
+
+Route::get('oauth/token/introspection', 'App\Http\Controllers\AuthController@introspection')
+->middleware('oauth.introspection')
+->name('introspection');
